67443ce469b18e198a2c55a402b8f6e8ae2de7e0
Satyamkumar-003 <satyamkumar0238@gmail.com>
2025-07-04 23:34:12 +0530

    Time: 4 ms (36.05%), Space: 10 MB (50.21%) - LeetHub

---
+++ 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    char kthCharacter(int k) {
+         string word = "a";
+    
+    while (word.size() < k) {
+        string next = "";
+        for (char c : word) {
+            if (c == 'z') {
+                next += 'a';
+            } else {
+                next += c + 1;
+            }
+        }
+        word += next;
+    }
+    
+    return word[k - 1]; 
+
+    }
+};
